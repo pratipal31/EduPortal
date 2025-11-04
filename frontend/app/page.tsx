da884b6a17f9b219d77efe1861e85d042f5e5dc9
pratipal31 <pratipal.dhaulakhandi.31@gmail.com>
2025-11-04 19:37:19 +0530

    initial commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import {AnimatedSignIn}from "../components/pages/Login";
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <AnimatedSignIn />
+    </div>
+  );
+}
